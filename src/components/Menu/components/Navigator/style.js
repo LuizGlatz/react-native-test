65569772a07d5de9ed417ad07537e54b72cf3725
Luiz Glatz <luizglatz@gmail.com>
2022-05-19 09:27:59 -0300

    redução do tamanho do menu

--- src/components/Menu/components/Navigator/style.js
+++ src/components/Menu/components/Navigator/style.js
@@ -6,7 +6,13 @@ export default StyleSheet.create({
     top: 0,
     zIndex: 3,
     backgroundColor: '#f8f8f8',
-    width: Dimensions.get('window').width,
+    width: Dimensions.get('window').width * 0.8,
     height: Dimensions.get('window').height,
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    shadowOffset: {
+      width: 10
+    },
+    elevation: 10
   },
 });
